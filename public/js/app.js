0da27b7e73774fe9d671f84c739750eee327e84e
Sthefany Floriano <Sthefany Floriano>
2016-11-10 15:08:51 -0500

    previous y next

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,54 @@
-/*document.getElementById("boton").addEventListener("click", function () {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange =
-		function () {
+var template = '<div class="row">' + '<div class="col-sm-12 col-md-12">' +
+	'<div class="thumbnail">' +
+	'<div class="caption">' +
+	'<h3> Hi, my name is __name__</h3>' +
+	'<p><a data-show-url="__url__" class="about btn btn-primary" role="button">Ver más</a><a href="#" class="btn btn-default" role="button">Button</a></p>' +
 
-		};
-	var callback = function () {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			document.getElementById("ajax").innerHTML = xhr.responseText;
+	'</div>' +
+	'</div>' +
+	'</div>' + '</div>';
+
+$(document).ready(function () {
+
+	var formatResponse = function (response) {
+		$("#total").text(response.results.length);
+		var personajes = "";
+		$.each(response.results, function (i, personaje) {
+			personajes += template
+				.replace("__name__", personaje.name)
+				.replace("__url__", personaje.url);
+
+		});
+		$("#people").html(personajes);
+		$("#next").attr("data-url", response.next);
+		$("#previous").attr("data-url",response.previous);
+
+		if (!response.next) {
+			$("#next").fadeOut();
+		}else{
+			$("#next").fadeIn();
+		}
+		if(!response.previous){
+			$("#previous").fadeOut();
+		}else{
+			$("#previous").fadeIn();
 		}
 	};
 
-	xhr.addEventListener('readystatechange', callback);
-	var url = "http://localhost:8080/demo.html"
-	xhr.open("GET", url);
-	xhr.send();
-});*/
+	$.getJSON("http://swapi.co/api/people/", formatResponse);
+
+	$("#next").click(function (e) {
+		e.preventDefault();
+		var url = $(this).attr("data-url");
+		$.getJSON(url, formatResponse);
+	});
+	$("#previous").click(function(e){
+		e.preventDefault();
+		var url = $(this).attr("data-url");
+		$.getJSON(url, formatResponse);
+	});
+	$("#people").on("click",".about",function(e){
+		e.preventDefault();
+		alert("Hola");
+	});
+});
